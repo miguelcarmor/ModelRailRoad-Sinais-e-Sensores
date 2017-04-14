a4fd4cac31f83ae30c49f67bf68a484cdc63f867
miguelcarmor <miguelcarmor@gmail.com>
2017-04-14 20:37:09 +0100

    Update MRR-PositionSensors.ino

--- MRR-PositionSensors.ino
+++ MRR-PositionSensors.ino
@@ -4,9 +4,8 @@
   Identifica a posição e direcção de uma locomotiva e activa as luzes de aviso.
   Limitada a uma locomotiva
 
-  Este Sketch faz com que o Arduino detecte, através de sensores Hall Effect,
-  a passagem de um comboio por um determinado ponto na linha e de seguida executa
-  uma série de tarefas.
+  Este Sketch detecta, através de sensores Hall Effect, a passagem de um comboio por um
+  determinado ponto na linha e de seguida executa uma série de tarefas.
 
   Quando a locomotiva, com um íman colado no fundo, activa um sensor, o sketch
   faz o seguinte:
@@ -21,21 +20,21 @@
   Esta versão só está a trabalhar com 5 sensores mas o procedimento é o mesmo até que se esgotem os pins disponiveis do arduino
 */
 
-#define n_sensor 5 //Insira aqui o úmero de sensores com que vai operar no layout
+#define n_sensor 5 //Insira aqui o número de sensores com que vai operar no layout
 
 int HallPin [n_sensor] = {18, 17, 16, 15, 14};     //Definir a que pinos estão ligados os sensores
-int HallId[n_sensor] = {1, 2, 3, 4, 5};           //Criei esta variável para conseguir visualizar a numeração correcta (para os humanos) dos sensores no serial monitor
 int LedGreen [n_sensor] = {4, 5, 6, 7, 8};        //Definir a que pinos estão ligados os leds verdes
 int LedRed [n_sensor] = {9, 10, 11, 12, 13};      //Definir a que pinos estão ligados os leds verdes
+
 int i;
 
 int HallPinStatus [n_sensor];                     //Variável que conterá o estado de cada um dos sensores após cada ciclo
+int HallId[n_sensor];           //Identificação dos sensores para leitura "Humana"
 
 int Position = 0;       //Variável que conterá o numero dos sensores que foram activados
-int LastPosition = 0;   //Variável que conterá o numero dos sensores que foram activados
+int LastPosition = 0;   //Variável que conterá o numero dos sensores que foram activados anteriormente
 
-int Direction = 0;
-//Indica qual a direcção. Sentido ascendente (2) Sentido descendente (1)
+int Direction = 0;      //Indica qual a direcção. Sentido ascendente (2) Sentido descendente (1)
 
 
 void setup() {
@@ -43,7 +42,7 @@ void setup() {
   //Abrir a porta série
   Serial.begin(115200);
 
-  //Definir pins e ligar leds
+  //Definir pins e ligar leds verdes
   for (i = 0; i < n_sensor; i++) {
     pinMode (HallPin[i], INPUT);          // Definir os PINs dos sensores (14 a 18)
     pinMode (LedGreen[i], OUTPUT);        // Definir os PINs dos leds verdes (4 a 8)
@@ -52,51 +51,54 @@ void setup() {
     digitalWrite (LedGreen[i], HIGH);     // ligar os leds verdes - No arranque liga todos os LEDS em modo de linha livre até ao primeiro contacto com um sensor.
   }
 
+  // Popular a variável com os numeros correctos
+  for (i = 0; i < n_sensor; i++) {
+    HallId[i] = i + 1;
+  }
 }
 
 
+  void loop() {
 
-void loop() {
+    // Ler o estado dos sensores
+    for (i = 0; i < n_sensor; i++) {
+      HallPinStatus [i] = digitalRead(HallPin[i]);
+      //SensorStatus(); //Se quiser visualizar o estado dos sensores em todos os ciclos apague "//" no inicio desta linha
+    }
 
-  // Ler o estado dos sensores
-  for (i = 0; i < 5; i++) {
-    HallPinStatus [i] = digitalRead(HallPin[i]);
-    //SensorStatus(); //Se quiser visualizar o estado dos sensores em todos os ciclos apague "//" no inicio desta linha
-  }
-
-//Determinar qual o sensor que foi activado e executar a respectiva função.
-  for(i=0;i<n_sensor;i++) {
+    //Determinar qual o sensor que foi activado e executar a respectiva função.
+    for (i = 0; i < n_sensor; i++) {
       if (HallPinStatus[i] == LOW) {
-    SensorExecute();
-    Position = i+1;
-  }
-  }
+        SensorExecute();
+        Position = i + 1;
+      }
+    }
 
-  //Estabelecer a direcção seguida
-  if (Position < LastPosition) {
-    Direction = 1;
-  }
-  else if (Position > LastPosition) {
-    Direction = 2;
-  }
-  else {
-    Direction = 0;
-  }
+    //Estabelecer a direcção seguida
+    if (Position < LastPosition) {
+      Direction = 1;
+    }
+    else if (Position > LastPosition) {
+      Direction = 2;
+    }
+    else {
+      Direction = 0;
+    }
 
 
-  //Estabelecer e manter a ultima posição conhecida
-  if (Position != LastPosition) {
+    //Estabelecer e manter a ultima posição conhecida
+    if (Position != LastPosition) {
+
+      //Chamar a função que envia para o serial monitor o status das variáveis
+      //O estado das variáveis. Só são enviados para o serial monitor quando existe uma alteração de posição
+      DisplayStatus();
+
+      //Redefinir as variáveis em função das novas alterações
+      LastPosition = Position;
+
+    }
 
     //Chamar a função que envia para o serial monitor o status das variáveis
-    //O estado das variáveis. Só são enviados para o serial monitor quando existe uma alteração de posição
-    DisplayStatus();
-
-    //Redefinir as variáveis em função das novas alterações
-    LastPosition = Position;
+    //StatusDisplay(); //Se quiser visualizar todos os ciclos no serial monitor apague "//" antes da função StatusDisplay
 
   }
-
-  //Chamar a função que envia para o serial monitor o status das variáveis
-  //StatusDisplay(); //Se quiser visualizar todos os ciclos no serial monitor apague "//" antes da função StatusDisplay
-
-}
